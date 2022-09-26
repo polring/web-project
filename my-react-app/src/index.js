b9811eb4d36813ff1f4497a71f10723b37f32c5a
전민제 <95161834+polring@users.noreply.github.com>
2022-09-27 01:15:12 +0900

    Add files via upload

---
+++ my-react-app/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));//id= 'root'인 Dom 객체를 가져온다.
+
+root.render( //App을 랜더링하는 함수
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
